99b0058af01ce8c628086c6e110f5549b5962499
Richard Fontana <fontana@sharpeleven.org>
2013-08-14 00:25:06 -0400

    Modify MainActivity etc.

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -2,12 +2,13 @@ package io.sabina.glossology
 
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
+import _root_.android.view.Menu
 
 class MainActivity extends Activity with TypedActivity {
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
-    findView(TR.textview).setText("hello world!")
+    findView(TR.textview).setText("Hello Lucy!")
   }
 }
