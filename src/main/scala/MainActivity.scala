a97a8654869f0c13364741679d8da28a6cf152fe
Richard Fontana <fontana@sharpeleven.org>
2013-08-17 01:18:24 -0400

    Modify activity to provide sample buttonTTS events

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -1,14 +1,50 @@
 package io.sabina.glossology
 
-import _root_.android.app.Activity
-import _root_.android.os.Bundle
-import _root_.android.view.Menu
+import android.app.Activity
+import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.view.View
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
 
-class MainActivity extends Activity with TypedActivity {
+import java.util.Locale
+
+class MainActivity extends Activity with TypedActivity with OnInitListener with OnClickListener {
+
+  private var myTTS : TextToSpeech = _
+  lazy val myDataCheckCode = 0
+  lazy val speakButton1 = findViewById(R.id.button1).asInstanceOf[Button]
+  lazy val speakButton2 = findViewById(R.id.button2).asInstanceOf[Button]
+  lazy val speakButton3 = findViewById(R.id.button3).asInstanceOf[Button]
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
-
-   // findView(TR.textview).setText("Hello Lucy!")
+    myTTS = new TextToSpeech(this, this)
+    speakButton1.setOnClickListener(this)
+    speakButton2.setOnClickListener(this)
+    speakButton3.setOnClickListener(this)
   }
+
+  private def speakWords(speech: String) {
+    myTTS.speak(speech, TextToSpeech.QUEUE_FLUSH, null)
+  }
+
+  override def onClick(v: View) {
+    if (v == speakButton1)
+      speakWords(getString(R.string.button_i_want))
+    else if (v == speakButton2)
+      speakWords(getString(R.string.button_to_watch))
+    else if (v == speakButton3)
+      speakWords(getString(R.string.button_Olivia))
+  }
+
+  def onInit(initStatus: Int) {
+    if (initStatus == TextToSpeech.SUCCESS) {
+      val result = myTTS setLanguage Locale.US
+    }
+  }
+
 }
+
